8347b441b9b2ba7228c9750b820b4072a4f15a7c
RAF-coder-py <ralph.honhon@icloud.com>
2024-08-29 18:15:42 +0200

    rien

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from prefect import task, flow
-import os
+# import os
 
 @flow(log_prints=True)
 def Game():
-    os.system('python3 Jeu/main.py')
+    # os.system('python3 Jeu/main.py')
+    print("Le jeu est lancé")
+
