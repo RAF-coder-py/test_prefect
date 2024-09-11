55af864b904dd6d5806d2fe36d1e05e9e9c52143
RAF-coder-py <ralph.honhon@icloud.com>
2024-09-11 20:00:35 +0200

    add rich

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from prefect import task, flow
+from prefect import flow
+from rich import print
 # import os
 
 @flow(log_prints=True)
 def Game():
     # os.system('python3 Jeu/main.py')
-    print("Le jeu est lancé")
+    print("[red]Le jeu est lancé[/red]")
 
